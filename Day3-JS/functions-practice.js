376419b4e2de936e572a5e499c9ce3b37ab764f5
sudeep <sudeepbr5364@gmail.com>
2026-03-05 17:52:32 +0530

    Day 3 - Functions and Number Analyzer Mini Project

--- Day3-JS/functions-practice.js
+++ Day3-JS/functions-practice.js
@@ -13,6 +13,26 @@ function checkeven(num){
         return "Odd";
     }
 }
-console.log( greet("Sudeep"));
+console.log(greet("Sudeep"));
 console.log(add(5, 7));
 console.log(checkeven(10));
+
+/*Arrow function*/
+const add1 = (a,b) => a + b;
+console.log(add1(5,3));
+
+const greet1= (name) => "Hello " + name;
+console.log(greet1("Sudeep"));
+
+const checkNumber = (num) =>{
+    if(num > 0){
+       return "Positive";
+    }else if(num < 0){
+      return  "Negative";
+    }else{
+       return  "Zero";     
+    }
+}
+console.log(checkNumber(10));
+console.log(checkNumber(-5));
+console.log(checkNumber(0));
---
+++ Day3-JS/number-analyzer.js
@@ -0,0 +1,33 @@
+function checktype(num){
+    if(num > 0){
+        return "Positive";
+    }
+    else if(num < 0){
+        return "Negative";
+    }
+    else{
+        return "Zero"
+    }
+}
+const checkEvenOdd = (num) => {
+    if( num % 2 === 0){
+        return "Even";
+    }
+    else{
+        return "Odd";
+    }
+}
+function MultiplyBy2(num){
+    return num *2;
+}
+
+let numbers= [10,-5,0,7];
+for(let i = 0; i < numbers.length; i++){
+    let num = numbers[i];
+console.log("Number: "+num);
+console.log("Type: "+checktype(num));
+console.log("Even/Odd: "+checkEvenOdd(num));
+console.log("MultiplyBy2: "+MultiplyBy2(num));
+console.log("--------------------------");
+}
+
